8cc4086a28157a38f7d036f4faeeab6ff07eee0b
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2018-03-11 15:28:14 -0400

    localhost

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ from snapgen import app, app_path, memcache, fifo
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8888,debug=True,processes=1)
+    app.run(port=8888,debug=True,processes=1)
