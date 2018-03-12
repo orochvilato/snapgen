8657fd5c012dccdb4f82e863d9112801d0989851
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2018-03-12 06:51:07 -0400

    stegano

--- snapgen/views/api.py
+++ snapgen/views/api.py
@@ -2,13 +2,17 @@
 from snapgen import app, app_path, memcache, fifo
 from snapgen.tools import image_response
 from flask import request
+from werkzeug.utils import secure_filename
 from queue import Queue
 from selenium import webdriver
 from selenium.webdriver.chrome import service
 from PIL import Image,ImageChops,ImageFont,ImageDraw
 from io import BytesIO
 import time
+from stegano import lsbset
+from stegano.lsbset import generators
 from threading import Thread
+
 q = Queue()
 
 nbworkers = 2
@@ -23,7 +27,7 @@ def getSnapshot(url,width,height,name,key,visuel,watermark):
     options = webdriver.ChromeOptions()
     options.binary_location = '/usr/bin/google-chrome'
     options.add_argument('headless')
-
+    print(watermark)
     options.add_argument('window-size=%dx%d' % (width,height+100))
     cdservice = service.Service('/usr/bin/chromedriver')
     states[key] = {'etat':u'Génération du visuel','avancement':20}
@@ -45,9 +49,18 @@ def getSnapshot(url,width,height,name,key,visuel,watermark):
     cdservice.stop()
     output = BytesIO()
     im2.save(output,'PNG')
+
+    # Watermark / stegano
+
+    import json
+    output.seek(0)
+    secret_image = lsbset.hide(output, watermark, generators.eratosthenes())
+    output_final = BytesIO()
+    secret_image.save(output_final,'PNG')
+
     states[key] = {'etat':u'Génération du visuel','avancement':100}
 
-    return output.getvalue()
+    return output_final.getvalue()
 
 
 
@@ -103,9 +116,46 @@ def prepare():
     item = {
         'url':request.form.get('url'),
         'name':request.form.get('name'),
-        'width':int(request.form.get('width') or 1024), 'height':int(request.form.get('height') or 1024), 'visuel':request.form.get('visuel'),'key':key,'watermark':request.form.get('watermark')}
+        'width':int(request.form.get('width') or 1024),
+        'height':int(request.form.get('height') or 1024), 'visuel':request.form.get('visuel'),'key':key,'watermark':request.form.get('watermark')}
 
     states[key] = {'etat':'En attente','avancement':0}
     keyqueue.append(key)
     q.put(item)
     return key
+
+ALLOWED_EXTENSIONS = ['png']
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/checkfile', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            fic = BytesIO()
+            file.save(fic)
+            fic.seek(0)
+            return lsbset.reveal(fic, generators.eratosthenes())
+
+
+    return '''
+    <!doctype html>
+    <title>Contrôle Image</title>
+    <h1>Contrôle image</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
